8442c5cb82082ffb46cb398d513328a11a2b173d
Alan R <alanribeiroandrade@gmail.com>
2017-09-19 15:48:15 +0200

    Solutions for exercises 03 and 04.

---
+++ ex03/ex03b.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+
+#include <alchemy/task.h>
+#include <alchemy/timer.h>
+#include <alchemy/sem.h>
+
+#define ITER 10
+
+static RT_TASK  t1;
+static RT_TASK  t2;
+static RT_TASK  t3;
+static RT_TASK  t4;
+
+int global = 0;
+RT_SEM contr, contr2; 
+
+
+void taskOne(void *arg)
+{
+    int i, num;
+    num = * (int *) arg;
+    for (i=0; i < ITER; i++) {
+        rt_sem_p(&contr, 10000000000L);
+        printf("I am taskTwo and global (%d)) = %d----------------\n", num, ++global);
+        rt_sem_v(&contr2);
+        
+        
+    }
+}
+
+void taskTwo(void *arg)
+{
+    int i, num;
+    num = * (int *) arg;
+    for (i=0; i < ITER; i++) {
+        rt_sem_p(&contr2, 10000000000L);
+        printf("I am taskTwo and global (%d)) = %d----------------\n", num , --global);
+        rt_sem_v(&contr);
+        
+    }
+}
+
+int main(int argc, char* argv[]) {
+
+    int index = 1;
+    rt_sem_create(&contr, "whatever", 1, S_FIFO);
+    rt_sem_create(&contr2, "whatever2", 1, S_FIFO);
+    rt_task_create(&t1, "task1", 0, 1, 0);
+    rt_task_create(&t2, "task2", 0, 1, 0);
+    rt_task_create(&t3, "task3", 0, 1, 0);
+    rt_task_start(&t1, &taskOne, &index);
+    index++;
+    rt_task_start(&t2, &taskTwo, &index);
+    index++;
+    rt_task_start(&t3, &taskOne, &index);
+    return 0;
+}
---
+++ ex03/ex03c-p.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/mman.h>
+
+#include <alchemy/task.h>
+#include <alchemy/sem.h>
+
+#define N 5
+
+RT_TASK tasks[N];
+RT_SEM contr;
+
+void demo(void *arg) {
+    
+    rt_sem_p(&contr, TM_INFINITE);
+    RT_TASK_INFO curtaskinfo;
+    rt_task_inquire(NULL,&curtaskinfo);
+    int num = * (int *) arg;
+    rt_printf("\nTask name: %s\nArgument: %d\n", curtaskinfo.name, num);
+    rt_sem_v(&contr);
+    
+
+}
+
+int main(int argc, char* argv[])
+{
+  char  str[10] ;
+  int i, prio = 10, index = 100;
+
+  // Perform auto-init of rt_print buffers if the task doesn't do so
+  //rt_print_auto_init(1);
+
+  // Lock memory : avoid memory swapping for this program
+  mlockall(MCL_CURRENT|MCL_FUTURE);
+
+  rt_printf("start tasks\n");
+
+  /*
+   * Arguments: &task,
+   *            name,
+   *            stack size (0=default),
+   *            priority,
+   *            mode (FPU, start suspended, ...)
+   */
+   rt_sem_create(&contr, "mySem", 0, S_PRIO);
+    
+  for(i = 0; i < N; i++){
+    long long unsigned createTime, startTime;
+    
+    sprintf(str,"%d",(i+1));
+    //saves the time in which the task was created;
+    createTime = rt_timer_read();
+    rt_task_create(&tasks[i], str, 0, prio, 0);
+    rt_task_start(&tasks[i], &demo, &index);
+    //saves the time in which the task was started;
+    startTime = rt_timer_read();
+    rt_printf("Creation time [%d]: %llu | Start time [%d]: %llu \n", i+1 ,createTime, i+1 , startTime);
+    index += 10;
+    prio += 10;
+
+  }
+
+  rt_sem_v(&contr);
+
+
+  rt_printf("\nEnd of execution\n");
+}
