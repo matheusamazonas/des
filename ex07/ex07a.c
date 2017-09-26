197c51ff0203cb566cef20fafc087c7fc9d849c2
Matheus Andrade <matheus.amazonas@gmail.com>
2017-09-26 16:03:42 +0200

    Exercise 7 with error handling

--- ex07/ex07a.c
+++ ex07/ex07a.c
@@ -1,64 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <errno.h>
 #include <alchemy/task.h>
 #include <alchemy/timer.h>
 #include <rtdm/gpio.h>
 
-#define EXECTIME   10e9   // execution time in ns
-#define SPINTIME   5e8   // spin time in ns
+#define PERIOD 500000000L
 
-RT_TASK t1;
-
-
-void demo(void *arg)
+typedef struct 
 {
-    RTIME starttime, runtime;
-    int fd,value, ret;
-    
-    fd=open("/dev/rtdm/pinctrl-bcm2835/gpio22",O_WRONLY);
-    runtime = 0;
-    while(runtime < EXECTIME) {
-        rt_timer_spin(SPINTIME);  // spin cpu doing nothing
+    int pin;
+    int value;
+} writeInfo;
 
-        if(runtime %1000000000 == 0){
-            value=1; // set gpio pin HIGH; LOW is 0
-            rt_printf("Led ON\n");
-            
-        }
-        else{
-            value = 0;
-            rt_printf("Led OFF\n");
-        }
-        
-        //set device to output mode with special device request GPIO_RTIOC_DIR_OUT
-        ret=ioctl(fd, GPIO_RTIOC_DIR_OUT, &value);
-        ret=write(fd, &value, sizeof(value));
-        
-        
-        runtime = runtime + SPINTIME;
 
-      
-      
+void writeValue(writeInfo* info)
+{
+    int fd, ret, errorCode;
+    char pinFile[50];
+    sprintf(pinFile, "/dev/rtdm/pinctrl-bcm2835/gpio%d", info->pin);   
+    fd = open(pinFile, O_WRONLY);
+    //set device to output mode with special device request GPIO_RTIOC_DIR_OUT
+    ret = ioctl(fd, GPIO_RTIOC_DIR_OUT, &info->value);
+    if (ret < 0)
+    {
+        errorCode = -ret;
+        printf("Error when trying to control the device %d. Error code: %d %s\n", info->pin, errorCode, strerror(errorCode));
     }
-
-
-    
+    ret = write(fd, &info->value, sizeof(info->value));   
+    if (ret < 0)
+    {
+        errorCode = -ret;
+        printf("Error when trying to write to the device %d. Error code: %d %s\n", info->pin, errorCode, strerror(errorCode));
+    }
 }
 
-int main(int argc, char* argv[]){
-
-	char  str[10] ;
-
-	sprintf(str,"myTask");
-	rt_task_create(&t1, str, 0, 50, 0);
-	rt_task_start(&t1, &demo, 0);
-
-
-	
-	
-    
-	
-
+void periodic(void *arg)
+{
+  writeInfo info = { .pin = 22, .value = 0};
+  while (1) {
+    writeValue(&info);  
+    rt_task_wait_period(NULL);
+    info.value = !info.value;
+  }
+  return;
 }
 
+int main (int argc, char* argv[])
+{
+    RT_TASK task;
+
+    rt_task_create(&task, "task1", 0, 50, 0);
+    rt_task_set_periodic(&task, TM_NOW, PERIOD);
+    rt_task_start(&task, &periodic, 0);
+
+    pause();
+}
--- ex07/ex07c.c
+++ ex07/ex07c.c
@@ -1,84 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <errno.h>
 #include <alchemy/task.h>
 #include <alchemy/timer.h>
 #include <rtdm/gpio.h>
 
-#define EXECTIME   10e9   // execution time in ns
-#define SPINTIME   5e8   // spin time in ns
+#define PERIOD 500000000L
 
-RT_TASK t1;
-
-
-void demo(void *arg)
+typedef struct 
 {
-    RTIME starttime, runtime;
-    int flag = 0;
-    // let the task run RUNTIME ns in steps of SPINTIME ns
-    runtime = 0;
+    int pin;
+    int value;
+}  writeInfo;
 
-	int fd2,value2, ret2;
-    int fd,value;
-    fd=open("/dev/rtdm/pinctrl-bcm2835/gpio22",O_WRONLY);
-	fd2=open("/dev/rtdm/pinctrl-bcm2835/gpio23",O_RDONLY);
-    int xeno_trigger=GPIO_TRIGGER_EDGE_FALLING|GPIO_TRIGGER_EDGE_RISING;
-    ret2=ioctl(fd2, GPIO_RTIOC_IRQEN, &xeno_trigger);
+void writeValue(writeInfo* info);
+
+void readInterruption(void* arg)
+{
+    int fd, pin, ret, value, errorCode, counter;
+    char pinFile[50];
     
-    int count = 0;
-    
-    while(read(fd2, &value2, sizeof(value2)) != -1){
-        
-        count++;
-        printf("interruptions counter %d\n", count);
-        value = count % 2;
-        ioctl(fd, GPIO_RTIOC_DIR_OUT, &value);
-        write(fd, &value, sizeof(value));
+    pin = * (int*) arg;
+    sprintf(pinFile, "/dev/rtdm/pinctrl-bcm2835/gpio%d", pin);   
+    fd = open(pinFile, O_RDONLY);
+    //set device to input mode with special device request GPIO_RTIOC_DIR_IN
+    int xeno_trigger = GPIO_TRIGGER_EDGE_FALLING|GPIO_TRIGGER_EDGE_RISING;
+    ret = ioctl(fd, GPIO_RTIOC_IRQEN, &xeno_trigger); 
+    if (ret < 0)
+    {
+        errorCode = -ret;
+        printf("Error when trying to control the device %d. Error code: %d %s\n", pin, errorCode, strerror(errorCode));
     }
-
-
-    rt_printf("end here");
-    
+    else
+    {
+        counter = 1;
+        while (read(fd, &value, sizeof(value)) >= 0)
+        {
+            writeInfo info = { .pin = 22, .value = value};
+            writeValue(&info);
+            printf("Number of interruptions: %d\n", counter);
+            counter++;
+        }
+    }
 }
 
-int main(int argc, char* argv[]){
-
-	char  str[10] ;
-RTIME starttime, runtime;
-	sprintf(str,"myTask");
-	rt_task_create(&t1, str, 0, 50, 0);
-	rt_task_start(&t1, &demo, 0);
-// runtime = 0;
-// 	int fd,value, ret;
-    
-//     fd=open("/dev/rtdm/pinctrl-bcm2835/gpio22",O_WRONLY);
-    
-//     while(runtime < EXECTIME) {
-// 		rt_timer_spin(SPINTIME);  // spin cpu doing nothing
-
-// 		if(runtime %1000000000 == 0){
-// 			value=1; // set gpio pin HIGH; LOW is 0
-// 			printf("Led ON\n");
-      		
-//     	}
-//     	else{
-//     		value = 0;
-//     		printf("Led OFF\n");
-//     	}
-    	
-// 		//set device to output mode with special device request GPIO_RTIOC_DIR_OUT
-// 		ret=ioctl(fd, GPIO_RTIOC_DIR_OUT, &value);
-// 		ret=write(fd, &value, sizeof(value));
-	    
-    	
-//     	runtime = runtime + SPINTIME;
-
-      
-      
-//     }
-    printf("\nType CTRL-C to end this program\n\n" );
-	pause();
-	
-
+void writeValue(writeInfo* info)
+{
+    int fd, ret, errorCode;
+    char pinFile[50];
+    sprintf(pinFile, "/dev/rtdm/pinctrl-bcm2835/gpio%d", info->pin);   
+    fd = open(pinFile, O_WRONLY);
+    //set device to output mode with special device request GPIO_RTIOC_DIR_OUT
+    ret = ioctl(fd, GPIO_RTIOC_DIR_OUT, &info->value);
+    if (ret < 0)
+    {
+        errorCode = -ret;
+        printf("Error when trying to control the device %d. Error code: %d %s\n", info->pin, errorCode, strerror(errorCode));
+    }
+    ret = write(fd, &info->value, sizeof(info->value));   
+    if (ret < 0)
+    {
+        errorCode = -ret;
+        printf("Error when trying to write to the device %d. Error code: %d %s\n", info->pin, errorCode, strerror(errorCode));
+    }
 }
 
+int main (int argc, char* argv[])
+{
+    RT_TASK task1;
+    int pin = 23;
+
+    rt_task_create(&task1, "task1", 0, 50, 0);
+    rt_task_start(&task1, &readInterruption, &pin);
+
+    pause();
+}
